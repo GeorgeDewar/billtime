9f9717a7a62d280a8367aa6afea5d62772a0793e
George Dewar <george@dewar.co.nz>
2014-11-06 22:24:25 +1300

    Add ability to set time using button

--- billtime.ino
+++ billtime.ino
@@ -3,6 +3,16 @@
 
 rgb_lcd lcd;
 
+#define BUTTON_PIN        5  // Button
+
+#define LONGPRESS_LEN    25  // Min nr of loops for a long press
+#define DELAY            20  // Delay per loop in ms
+
+enum { EV_NONE=0, EV_SHORTPRESS, EV_LONGPRESS };
+
+boolean button_was_pressed; // previous state
+int button_pressed_counter; // press running duration
+
 unsigned long lastSecond = 0;
 unsigned int seconds = 0;
 unsigned int minutes = 0;
@@ -11,9 +21,24 @@ unsigned int hours = 0;
 void setup() {
     // set up the LCD's number of columns and rows:
     lcd.begin(16, 2);
+    pinMode(BUTTON_PIN, INPUT);
+    button_was_pressed = false;
+    button_pressed_counter = 0;
 }
 
 void loop() {
+    int event = handleButton();
+    
+    // Check if button pressed
+    switch (event) {
+      case EV_SHORTPRESS:
+        minutes++;
+        break;
+      case EV_LONGPRESS:
+        hours++;
+        break;
+    }
+  
     if(millis() - lastSecond > 1000)
     {
         lastSecond += 1000;
@@ -41,5 +66,28 @@ void loop() {
     lcd.print(".");
     lcd.print((millis() - lastSecond) / 100);
     lcd.print(" seconds ");
+}
+
+int handleButton()
+{
+  int event;
+  int button_now_pressed = digitalRead(BUTTON_PIN); // pin high -> pressed
+
+  if (!button_now_pressed && button_was_pressed) {
+    if (button_pressed_counter < LONGPRESS_LEN)
+      event = EV_SHORTPRESS;
+    else
+      event = EV_LONGPRESS;
+  }
+  else
+    event = EV_NONE;
+
+  if (button_now_pressed)
+    ++button_pressed_counter;
+  else
+    button_pressed_counter = 0;
+
+  button_was_pressed = button_now_pressed;
+  return event;
 }
 
